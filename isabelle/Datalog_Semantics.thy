2bc3c0d329c092f7ca937df9fc77d16db4e6a20f
Martin Kleppmann <martin@kleppmann.com>
2017-07-11 19:11:58 +0100

    Datalog ruleset evaluation semantics

--- isabelle/Datalog_Semantics.thy
+++ isabelle/Datalog_Semantics.thy
@@ -4,79 +4,70 @@ imports
   Main
   "~~/src/HOL/Library/Monad_Syntax"
 begin
-  
+
 subsection \<open>Semantics of Datalog rules\<close>
 
-datatype 'val element =
+datatype 'val dterm =
   Var "string" |
   Val "'val"
 
 datatype 'val atom =
-  Match "string" "'val element list"
+  Match "string" "'val dterm list" |
+  Equal "'val dterm" "'val dterm"
 
 record 'val rule =
   rule_rel    :: "string"
-  rule_params :: "string list"
+  rule_params :: "'val dterm list"
   rule_atoms  :: "'val atom set"
 
 type_synonym 'val fact = "string \<times> 'val list"
 type_synonym 'val database = "'val fact set"
 type_synonym 'val valuation = "string \<Rightarrow> 'val option"
 
-fun match_vars :: "'val element list \<Rightarrow> 'val list \<Rightarrow> 'val valuation option" where
-  "match_vars [] [] = Some Map.empty" |
-  "match_vars (Val x # vars) (val # vals) =
-     (if x = val then match_vars vars vals else None)" |
-  "match_vars (Var v # vars) (val # vals) = do {
-     mapping \<leftarrow> match_vars vars vals;
-     case mapping v of
-       None   \<Rightarrow> Some (mapping (v \<mapsto> val)) |
-       Some x \<Rightarrow> if x = val then Some mapping else None
-   }" |
-  "match_vars _ _ = None"
+definition subst_term :: "'val dterm \<Rightarrow> 'val valuation \<Rightarrow> 'val option" where
+  "subst_term term mapping \<equiv> case term of
+     Val x \<Rightarrow> Some x |
+     Var v \<Rightarrow> mapping v"
 
-fun subst_vars :: "'val element list \<Rightarrow> 'val valuation \<Rightarrow> 'val list option" where
+fun subst_vars :: "'val dterm list \<Rightarrow> 'val valuation \<Rightarrow> 'val list option" where
   "subst_vars [] mapping = Some []" |
-  "subst_vars (elem#vars) mapping = do {
-     values \<leftarrow> subst_vars vars mapping;
-     case elem of
-       Val x \<Rightarrow> Some (x#values) |
-       Var v \<Rightarrow> do { x \<leftarrow> mapping v; Some (x#values) }
+  "subst_vars (term#terms) mapping = do {
+     xs \<leftarrow> subst_vars terms mapping;
+     x  \<leftarrow> subst_term term mapping;
+     Some (x#xs)
    }"
 
-definition match_atom :: "'val database \<Rightarrow> string \<Rightarrow> 'val element list \<Rightarrow> 'val database" where
-  "match_atom db rel vars \<equiv> {(rel',vars') \<in> db. rel = rel' \<and> match_vars vars vars' \<noteq> None}"
+fun match_vars :: "'val dterm list \<Rightarrow> 'val list \<Rightarrow> 'val valuation \<Rightarrow> bool" where
+  "match_vars [] [] mapping = True" |
+  "match_vars (term # terms) (val # vals) mapping =
+     (subst_term term mapping = Some val \<and> match_vars terms vals mapping)" |
+  "match_vars _ _ _ = False"
 
-fun satisfies :: "'val database \<Rightarrow> 'val atom \<Rightarrow> bool" (infix "\<Turnstile>" 60) where
-  "db \<Turnstile> Match rel vars = (match_atom db rel vars \<noteq> {})"
+definition match_atom :: "'val database \<Rightarrow> 'val atom \<Rightarrow> 'val valuation \<Rightarrow> bool" where
+  "match_atom db atom mapping \<equiv> case atom of
+     Match rel vars \<Rightarrow> \<exists>fact \<in> db. fst fact = rel \<and> match_vars vars (snd fact) mapping |
+     Equal v1  v2   \<Rightarrow> subst_term v1 mapping = subst_term v2 mapping"
 
-thm match_vars.cases
-thm match_vars.elims
-thm match_vars.pelims
-thm match_vars.induct
-thm match_vars.simps
-find_theorems name: match_vars
+definition deduce_rule :: "'val database \<Rightarrow> 'val rule \<Rightarrow> 'val fact \<Rightarrow> bool" where
+  "deduce_rule db rule derived \<equiv> \<exists>vals valuation. derived = (rule_rel rule, vals) \<and>
+     subst_vars (rule_params rule) valuation = Some vals \<and>
+     (\<forall>atom \<in> rule_atoms rule. match_atom db atom valuation)"
 
-lemma match_vars_consistent:
-  assumes "match_vars vars vals = Some mapping"
-  shows "subst_vars vars mapping = Some vals"
-using assms apply(induction vars arbitrary: vals mapping)
-  apply(metis list.exhaust match_vars.simps(6) option.distinct(1) subst_vars.simps(1))
-  apply(subgoal_tac "\<exists>x vals'. vals = x#vals'", (erule exE)+, simp)
-  prefer 2
-  apply(metis list.exhaust match_vars.simps(5) option.distinct(1))
-  apply(erule_tac x="vals'" in meta_allE)
-  apply(subgoal_tac "\<exists>map'. match_vars vars vals' = Some map'", erule exE, simp)
-  prefer 2
-  apply(case_tac a, simp, meson bind_eq_Some_conv)
-  apply(metis match_vars.simps(2) option.discI)
-  apply(erule_tac x="map'" in meta_allE, simp)
-  apply(subgoal_tac "subst_vars vars map' = subst_vars vars mapping")
-  apply(case_tac a, simp)
-  apply(subgoal_tac "mapping x1 = Some x", simp)
-  apply(case_tac "map' x1", force)
-  apply(case_tac "aa = x", simp, simp)
-  apply(simp, metis option.simps(3))
-  oops
+definition deduce_step :: "'val database \<Rightarrow> 'val rule set \<Rightarrow> 'val database" where
+  "deduce_step db rules \<equiv> db \<union> {derived. \<exists>rule \<in> rules. deduce_rule db rule derived}"
+
+inductive fixpoint :: "'val database \<Rightarrow> 'val rule set \<Rightarrow> 'val database \<Rightarrow> bool" where
+  "fixpoint db rules db" |
+  "\<lbrakk> fixpoint db rules prev; deduce_step prev rules = next \<rbrakk> \<Longrightarrow> fixpoint db rules next"
+
+(*
+Alternative definition that fails at "Proving the simplification rules ...":
+inductive fixpoint :: "'val database \<Rightarrow> 'val rule set \<Rightarrow> 'val fact \<Rightarrow> bool" where
+  "\<lbrakk> fact \<in> db \<rbrakk> \<Longrightarrow> fixpoint db rules fact" |
+  "\<lbrakk> rule \<in> rules; deduce_rule {f. fixpoint db rules f} rule derived \<rbrakk> \<Longrightarrow> fixpoint db rules derived"
+*)
+
+(*fun satisfies :: "'val database \<Rightarrow> 'val atom \<Rightarrow> bool" (infix "\<Turnstile>" 60) where
+  "db \<Turnstile> Match rel vars = (match_atom db rel vars \<noteq> {})"*)
 
 end
