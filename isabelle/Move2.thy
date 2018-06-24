c408687e0a91f66b4cc72ccc0b9916de23c9519b
Martin Kleppmann <martin@kleppmann.com>
2018-06-24 12:00:10 +0100

    Add an assumption to make unconditional_MakeChild work

--- isabelle/Move2.thy
+++ isabelle/Move2.thy
@@ -73,28 +73,17 @@ qed*)
 
 lemma unconditional_MakeChild:
   assumes "\<forall>i \<in> fst ` ops. i < oid"
-  shows "unconditional_spec (unconditional_query ops) (oid, MakeChild obj key) =
-         unconditional_query (ops \<union> {(oid, MakeChild obj key)})"
+    and "\<And>i ref. (i, Remove ref) \<in> ops \<Longrightarrow> ref < i"
+  shows "unconditional_query (ops \<union> {(oid, MakeChild obj key)}) =
+        (unconditional_query ops) \<union> {(oid, obj, key, Child oid)}"
 proof -
   obtain ops' where ops': "ops' = ops \<union> {(oid, MakeChild obj key)}"
     by fastforce
-  obtain elem :: "'a \<times> 'a node_ref \<times> 'b \<times> ('a, 'c) val"
-    where elem: "elem = (oid, obj, key, Child oid)"
-    by fastforce
-  have 1: "\<And>e. (\<exists>oid obj key ref. e = (oid, obj, key, Child ref) \<and>
-           ((oid, MakeChild obj key) \<in> ops \<and> ref = oid \<or> (oid, Move obj key ref) \<in> ops)) \<or>
-           e = elem \<longleftrightarrow>
-       (\<exists>oid obj key ref. e = (oid, obj, key, Child ref) \<and>
-           ((oid, MakeChild obj key) \<in> ops' \<and> ref = oid \<or> (oid, Move obj key ref) \<in> ops'))"
-    using ops' elem by blast
-  have 2: "\<And>e. (\<exists>oid obj key ref. e = (oid, obj, key, Child ref) \<and>
-           (\<nexists>i. (i, Remove oid) \<in> ops) \<and>
-           (\<nexists>i. oid < i \<and> (\<exists>obj' key'. (i, Move obj' key' ref) \<in> ops))) \<longleftrightarrow>
-       (\<exists>oid obj key ref. e = (oid, obj, key, Child ref) \<and>
-           (\<nexists>i. (i, Remove oid) \<in> ops') \<and>
-           (\<nexists>i. oid < i \<and> (\<exists>obj' key'. (i, Move obj' key' ref) \<in> ops')))"
-    by (simp add: ops')
-  from 1 2 have "\<And>e. (\<exists>oid obj key ref. e = (oid, obj, key, Child ref) \<and>
+  have no_remove: "\<nexists>i. (i, Remove oid) \<in> ops"
+    using assms(1,2) by fastforce
+  have "\<nexists>i. oid < i \<and> (\<exists>obj' key'. (i, Move obj' key' oid) \<in> ops)"
+    using assms(1) by auto
+  hence "\<And>e. (\<exists>oid obj key ref. e = (oid, obj, key, Child ref) \<and>
            ((oid, MakeChild obj key) \<in> ops \<and> ref = oid \<or> (oid, Move obj key ref) \<in> ops) \<and>
            (\<nexists>i. (i, Remove oid) \<in> ops) \<and>
            (\<nexists>i. oid < i \<and> (\<exists>obj' key'. (i, Move obj' key' ref) \<in> ops))) \<or>
@@ -103,14 +92,14 @@ proof -
            ((oid, MakeChild obj key) \<in> ops' \<and> ref = oid \<or> (oid, Move obj key ref) \<in> ops') \<and>
            (\<nexists>i. (i, Remove oid) \<in> ops') \<and>
            (\<nexists>i. oid < i \<and> (\<exists>obj' key'. (i, Move obj' key' ref) \<in> ops')))"
-    sorry
+    using no_remove ops' Pair_inject Un_insert_right insert_iff sup_bot.right_neutral
+      tree_op.distinct(5) by force
   moreover have "\<And>e. (\<exists>oid obj key val. e = (oid, obj, key, Val val) \<and>
            (oid, Assign obj key val) \<in> ops \<and> (\<nexists>i. (i, Remove oid) \<in> ops)) \<longleftrightarrow>
            (\<exists>oid obj key val. e = (oid, obj, key, Val val) \<and>
            (oid, Assign obj key val) \<in> ops' \<and> (\<nexists>i. (i, Remove oid) \<in> ops'))"
     by (simp add: ops')
-  ultimately have "\<And>e.
-     (\<exists>oid obj key ref. e = (oid, obj, key, Child ref) \<and>
+  ultimately have "\<And>e. (\<exists>oid obj key ref. e = (oid, obj, key, Child ref) \<and>
            ((oid, MakeChild obj key) \<in> ops \<and> ref = oid \<or> (oid, Move obj key ref) \<in> ops) \<and>
            (\<nexists>i. (i, Remove oid) \<in> ops) \<and>
            (\<nexists>i. oid < i \<and> (\<exists>obj' key'. (i, Move obj' key' ref) \<in> ops))) \<or>
@@ -123,7 +112,7 @@ proof -
            (\<nexists>i. oid < i \<and> (\<exists>obj' key'. (i, Move obj' key' ref) \<in> ops'))) \<or>
      (\<exists>oid obj key val. e = (oid, obj, key, Val val) \<and>
            (oid, Assign obj key val) \<in> ops' \<and> (\<nexists>i. (i, Remove oid) \<in> ops'))"
-    sorry
+    sorry (* sledgehammer finds "by metis", but it times out *)
   hence "{e. (\<exists>oid obj key ref. e = (oid, obj, key, Child ref) \<and>
            ((oid, MakeChild obj key) \<in> ops \<and> ref = oid \<or> (oid, Move obj key ref) \<in> ops) \<and>
            (\<nexists>i. (i, Remove oid) \<in> ops) \<and>
@@ -151,64 +140,87 @@ proof -
            (\<nexists>i. oid < i \<and> (\<exists>obj' key'. (i, Move obj' key' ref) \<in> ops))) \<or>
      (\<exists>oid obj key val. e = (oid, obj, key, Val val) \<and>
            (oid, Assign obj key val) \<in> ops \<and> (\<nexists>i. (i, Remove oid) \<in> ops))} \<union> {(oid, obj, key, Child oid)}"
-    using unconditional_query_def by blast
-  ultimately have "unconditional_query ops' = (unconditional_query ops) \<union> {(oid, obj, key, Child oid)}"
-    using unconditional_query_def by (metis (mono_tags, lifting))
-  moreover have "unconditional_spec (unconditional_query ops) (oid, MakeChild obj key) =
-        (unconditional_query ops) \<union> {(oid, obj, key, Child oid)}"
-    by simp
+    by blast
   ultimately show ?thesis
-    by (simp add: ops')
+    using unconditional_query_def ops' by (metis (mono_tags, lifting))
 qed
 
 lemma unconditional_Move:
   assumes "\<forall>i \<in> fst ` ops. i < oid"
-  shows "unconditional_spec (unconditional_query ops) (oid, Move obj key ref) =
-         unconditional_query (ops \<union> {(oid, Move obj key ref)})"
+  shows "unconditional_query (ops \<union> {(oid, Move obj key ref)}) =
+    {e \<in> unconditional_query ops. snd (snd (snd e)) \<noteq> Child ref} \<union> {(oid, obj, key, Child ref)}"
   sorry
 
 lemma unconditional_Assign:
   assumes "\<forall>i \<in> fst ` ops. i < oid"
-  shows "unconditional_spec (unconditional_query ops) (oid, Assign obj key val) =
-         unconditional_query (ops \<union> {(oid, Assign obj key val)})"
+  shows "unconditional_query (ops \<union> {(oid, Assign obj key val)}) =
+    unconditional_query ops \<union> {(oid, obj, key, Val val)}"
   sorry
 
 lemma unconditional_Remove:
   assumes "\<forall>i \<in> fst ` ops. i < oid"
-  shows "unconditional_spec (unconditional_query ops) (oid, Remove ref) =
-         unconditional_query (ops \<union> {(oid, Remove ref)})"
+  shows "unconditional_query (ops \<union> {(oid, Remove ref)}) =
+    {e \<in> unconditional_query ops. fst e \<noteq> ref}"
   sorry
 
 
-lemma unconditional_equiv:
+theorem unconditional_equiv:
   assumes "sorted (map fst ops)" and "distinct (map fst ops)"
+    and "\<And>i ref. (i, Remove ref) \<in> set ops \<Longrightarrow> ref < i"
   shows "foldl unconditional_spec {} ops = unconditional_query (set ops)"
 using assms proof(induction ops rule: List.rev_induct, simp add: unconditional_query_def)
   case (snoc x xs)
   hence "foldl unconditional_spec {} xs = unconditional_query (set xs)"
     using distinct_butlast sorted_butlast by (simp add: sorted_append)
+  moreover have remove_less: "\<And>i ref. (i, Remove ref) \<in> set xs \<Longrightarrow> ref < i"
+    by (simp add: snoc.prems(3))
   moreover obtain oid oper where x_pair: "x = (oid, oper)"
     by fastforce
-  hence id_max: "\<forall>i \<in> set (map fst xs). i < oid"
+  moreover from this have "\<forall>i \<in> set (map fst xs). i < oid"
     using snoc by (metis disjoint_insert(1) distinct_append empty_set fst_conv less_le
       list.set(2) list.simps(8) list.simps(9) map_append singletonI sorted_append)
+  hence id_max: "\<forall>i \<in> fst ` set xs. i < oid"
+    by simp
   moreover have "unconditional_spec (unconditional_query (set xs)) (oid, oper) =
                  unconditional_query (set xs \<union> {(oid, oper)})"
   proof(cases oper)
     case (MakeChild obj key)
-    then show ?thesis using unconditional_MakeChild id_max by fastforce
+    moreover have "unconditional_spec (unconditional_query (set xs)) (oid, MakeChild obj key) =
+        (unconditional_query (set xs)) \<union> {(oid, obj, key, Child oid)}"
+      by simp
+    moreover have "... = unconditional_query (set xs \<union> {(oid, MakeChild obj key)})"
+      using unconditional_MakeChild id_max remove_less by metis
+    ultimately show ?thesis
+      by blast
   next
     case (Move obj key ref)
-    then show ?thesis using unconditional_Move id_max by fastforce
+    moreover have "unconditional_spec (unconditional_query (set xs)) (oid, Move obj key ref) =
+        {e \<in> unconditional_query (set xs). snd (snd (snd e)) \<noteq> Child ref} \<union> {(oid, obj, key, Child ref)}"
+      by (simp add: do_move_def)
+    moreover have "... = unconditional_query (set xs \<union> {(oid, Move obj key ref)})"
+      using unconditional_Move id_max by fastforce
+    ultimately show ?thesis
+      by blast
   next
     case (Assign obj key val)
-    then show ?thesis using unconditional_Assign id_max by fastforce
+    moreover have "unconditional_spec (unconditional_query (set xs)) (oid, Assign obj key val) =
+        (unconditional_query (set xs)) \<union> {(oid, obj, key, Val val)}"
+      by simp
+    moreover have "... = unconditional_query (set xs \<union> {(oid, Assign obj key val)})"
+      using unconditional_Assign id_max by fastforce
+    ultimately show ?thesis
+      by blast
   next
     case (Remove ref)
-    then show ?thesis using unconditional_Remove id_max by fastforce
+    moreover have "unconditional_spec (unconditional_query (set xs)) (oid, Remove ref) =
+        {e \<in> unconditional_query (set xs). fst e \<noteq> ref}"
+      by simp
+    moreover have "... = unconditional_query (set xs \<union> {(oid, Remove ref)})"
+      using unconditional_Remove id_max by fastforce
+    ultimately show ?thesis
+      by blast
   qed
-  ultimately show ?case
-    using x_pair by auto
+  ultimately show ?case by auto
 qed
 
 
